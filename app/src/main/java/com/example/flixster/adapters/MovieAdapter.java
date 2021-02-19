3a186b693808d07fe71dd9851553e8d3b363d7dd
Rohit <ram6354@psu.edu>
2021-02-19 10:34:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,48 @@
+package com.example.flixster.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flixster.R;
+
+import java.util.List;
+
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    Context context;
+    List<Movie> movies;
+    @NonNull
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle=itemView.findViewById(R.id.tvTitle);
+            tvOverview=itemView.findViewById(R.id.tvOverview);
+            ivPoster =itemView.findViewById(R.id.ivPoster);
+
+        }
+    }
+}
